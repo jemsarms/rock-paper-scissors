5256b3867559045d91fd182f7c08700421ec6685
Jem <71848797+jemsarms@users.noreply.github.com>
2025-01-25 15:08:59 +0800

    Completed functionality of press to play on UI

--- index.js
+++ index.js
@@ -2,48 +2,63 @@ console.log("Hello World");
 
 let computerScore = 0;
 let humanScore = 0;
-let roundCount = 3;
+let playedRounds = 0;
+let roundCount = 5;
 
 const getComputerChoice = () => {
     let choice = Math.random().toFixed(1);
     return choice < 0.3 ? "Rock" : choice < 0.6 ? "Paper" : "Scissors";
 };
 
-const getHumanChoice = () => {
-    return prompt("Choose your weapon: Rock | Paper | Scissors");
-};
+const playRound = (playerChoice) => {
+    document.querySelector(".result-box").innerText;
+    document.querySelector("#playerScore").innerText;
+    document.querySelector("#compScore").innerText;
+    document.querySelector("#currentRound").innerText;
+    document.querySelector("#totalRounds").innerText;
+    let results;
 
-const playRound = (compChoice, playerChoice) => {
-    console.log(`Player Hand: ${playerChoice}\nComputer Hand: ${compChoice}`);
-    if (compChoice.toUpperCase() === playerChoice.toUpperCase()) {
-        console.log("This round is a draw");
-    } else if (
-        (playerChoice.toUpperCase() === "ROCK" && compChoice.toUpperCase() === "SCISSORS") ||
-        (playerChoice.toUpperCase() === "SCISSORS" && compChoice.toUpperCase() === "PAPER") ||
-        (playerChoice.toUpperCase() === "PAPER" && compChoice.toUpperCase() === "ROCK")
-    ) {
-        humanScore++;
-        return "Player wins!";
+    if (playedRounds < roundCount) {
+        const compChoice = getComputerChoice();
+
+        console.log(`Player Hand: ${playerChoice}\nComputer Hand: ${compChoice}`);
+        if (compChoice.toUpperCase() === playerChoice.toUpperCase()) {
+            playedRounds++;
+            results = `Match Draw! \n\n${playerChoice} cancels ${compChoice}`;
+        } else if (
+            (playerChoice.toUpperCase() === "ROCK" && compChoice.toUpperCase() === "SCISSORS") ||
+            (playerChoice.toUpperCase() === "SCISSORS" && compChoice.toUpperCase() === "PAPER") ||
+            (playerChoice.toUpperCase() === "PAPER" && compChoice.toUpperCase() === "ROCK")
+        ) {
+            humanScore++;
+            playedRounds++;
+            results = `You win! \n\n${playerChoice} beats ${compChoice}`;
+        } else {
+            computerScore++;
+            playedRounds++;
+            results = `Computer wins! \n\n${playerChoice} loses to ${compChoice}`;
+        }
+
+        document.querySelector("#playerScore").innerText = humanScore;
+        document.querySelector("#compScore").innerText = computerScore;
+        document.querySelector(".result-box").innerText = results;
+
+        //Update Round
+        document.querySelector("#currentRound").innerText = playedRounds;
+        document.querySelector("#totalRounds").innerText = roundCount;
     } else {
-        computerScore++;
-        return "Computer wins!";
+        checkRound();
     }
 };
 
-const playGame = () => {
-    for (let i = 1; i <= roundCount; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(computerSelection, humanSelection));
+const checkRound = () => {
+    if (playedRounds === roundCount) {
+        return humanScore > computerScore
+            ? "Player wins the Game"
+            : humanScore < computerScore
+            ? "Computer wins the Game"
+            : "The Game is a draw";
     }
-
-    console.log(`Player Score: ${computerScore}\nComputer Score: ${humanScore}`);
-
-    return humanScore > computerScore
-        ? "Player wins the Game"
-        : humanScore < computerScore
-        ? "Computer wins the Game"
-        : "The Game is a draw";
 };
 
 // console.log(playGame());
